f7b0c64237bb8a2022c17e271ad463676c9112fc
nickbaf <nickbaf@icloud.com>
2019-02-15 22:30:09 +0000

    implemented camera movement

---
+++ Assets/Script/Following.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Following : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector3 offset;
+
+
+    void Awake()
+    {
+
+        offset = transform.position - player.position;
+        
+    }
+
+    private void LateUpdate() //used for cameras
+    {
+        transform.position = player.position + offset;
+    }
+}
