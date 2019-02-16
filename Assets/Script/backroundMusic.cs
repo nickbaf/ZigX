1ef22c3b4fb48b98eb557c27fadc700b45e967c4
nickbaf <nickbaf@icloud.com>
2019-02-16 18:35:07 +0000

    added music

---
+++ Assets/Script/backroundMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backroundMusic : MonoBehaviour
+{
+
+    public static backroundMusic theMusic;
+    private void Awake()
+    {
+        if (theMusic == null)
+        {
+            theMusic = this;
+        }
+        else if(theMusic!=this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
